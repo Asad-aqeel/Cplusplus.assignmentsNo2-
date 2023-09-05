312eff50b9e0b5fddcd1f83aea352c7d69bd1e16
Asad-aqeel <144030369+Asad-aqeel@users.noreply.github.com>
2023-09-05 05:39:34 -0700

    Update QuesNo3.cpp

--- QuesNo3.cpp
+++ QuesNo3.cpp
@@ -3,25 +3,35 @@ using namespace std;
 
 int main()
 {
+    int income, days;
 
-    int days, income;
-
-    cout << "Enter days:";
-    cin >> days;
-
-    cout << "Enter income:";
+    cout << "Enter your income: $";
     cin >> income;
+
+    cout << "Enter the number of days: ";
+    cin >> days;
 
     if (days > 365)
     {
-        (income - 2.5);
-        income = (income * days) / 100;
-        cout << "The remaining income after zakat deduction is " << income <<"%" <<"." << endl;
+
+        int zakat = 0.025 * income;
+        income -= zakat;
+        cout << "2.5% Zakat deducted: $" << zakat << endl;
     }
 
-    else
+    switch (days < 365)
+    {
+    case 1:
     {
         cout << "Days are less then a year." << endl;
+    }
+
+    break;
+
+    default:
+
+        cout << "Your remaining income after zakat deduction: $" << income << "%" << endl;
+        break;
     }
 
     return 0;
