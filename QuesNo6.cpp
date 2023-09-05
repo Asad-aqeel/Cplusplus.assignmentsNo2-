03db40f43b0a8b2e3fdc594adf3ef75050b830b5
Asad-aqeel <144030369+Asad-aqeel@users.noreply.github.com>
2023-09-05 02:56:30 -0700

    Create QuesNo6.cpp

---
+++ QuesNo6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b, c;
+
+    cout << "Enter the value of a:";
+    cin >> a;
+
+    cout << "Enter the value of b:";
+    cin >> b;
+
+    c = a;
+    a = b;
+    b = c;
+
+    cout << "After swaping" << endl;
+    cout << "a=" << a << "." << endl;
+    cout << "b=" << b << "." << endl;
+
+    return 0;
+}
